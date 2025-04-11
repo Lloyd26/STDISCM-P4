af7ae2e20c1cb70a04ae6562ba9683e76e2003c9
Lloyd26 <ademic26@yahoo.com>
2025-04-11 20:03:38 +0800

    Add method in CourseSectionRepository for finding all sections by course using SQL statement

--- Course/src/main/java/stdiscm/p4/course/repository/CourseSectionRepository.java
+++ Course/src/main/java/stdiscm/p4/course/repository/CourseSectionRepository.java
@@ -1,9 +1,34 @@
 package stdiscm.p4.course.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import stdiscm.p4.course.model.CourseSection;
 
+import java.util.List;
+
 @Repository
 public interface CourseSectionRepository extends JpaRepository<CourseSection, Integer> {
+    
+    @Query(value = "SELECT\n" +
+            "cs.id AS `ClassNbr`,\n" +
+            "c.code AS `Course`,\n" +
+            "cs.section AS `Section`,\n" +
+            "CONCAT(f.first_name, ' ', f.last_name) AS `Faculty`,\n" +
+            "cs.enrollment_cap AS `EnrlCap`,\n" +
+            "(SELECT COUNT(*) FROM enrollments WHERE section_id=cs.id) AS `Enrolled`\n" +
+            "FROM enrollments AS e\n" +
+            "JOIN course_sections AS cs\n" +
+            "ON e.section_id=cs.id\n" +
+            "JOIN students AS s\n" +
+            "ON e.student_id=s.id_number\n" +
+            "JOIN faculty AS f\n" +
+            "ON cs.faculty_id=f.id_number\n" +
+            "JOIN courses AS c\n" +
+            "ON cs.course_id=c.id\n" +
+            "WHERE c.code=:courseCode\n" +
+            "ORDER BY cs.section ASC",
+    nativeQuery = true)
+    List<Object[]> findAllSectionsByCourse(@Param("courseCode") String courseCode);
 }
