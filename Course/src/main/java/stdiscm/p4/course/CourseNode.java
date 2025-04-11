4477d5cee4b7c611a048470736685cb7a706a89d
Lloyd26 <ademic26@yahoo.com>
2025-04-11 17:36:32 +0800

    Create Course node

---
+++ Course/src/main/java/stdiscm/p4/course/CourseNode.java
@@ -0,0 +1,13 @@
+package stdiscm.p4.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseNode {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseNode.class, args);
+	}
+
+}
---
+++ Course/src/test/java/stdiscm/p4/course/CourseNodeTests.java
@@ -0,0 +1,13 @@
+package stdiscm.p4.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseNodeTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
