4f281eac47093351f192f857051e19b07bf56e80
Lloyd26 <ademic26@yahoo.com>
2025-04-12 21:26:39 +0800

    Create Grades node

---
+++ Grades/src/main/java/stdiscm/p4/grades/GradesNode.java
@@ -0,0 +1,13 @@
+package stdiscm.p4.grades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradesNode {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradesNode.class, args);
+	}
+
+}
---
+++ Grades/src/test/java/stdiscm/p4/GradesNodeTests.java
@@ -0,0 +1,13 @@
+package stdiscm.p4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradesNodeTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
